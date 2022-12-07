d8431430646ceb6d68e575ceb63e07b3b3ac8b33
powai13 <76779356+powai13@users.noreply.github.com>
2022-12-07 12:13:32 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+window.onload = function () {
+
+    let cookie = document.cookie;
+    console.log(cookie);
+
+    // ボタンを押すと発火
+    function push()  {
+
+        // 入力したテキストを取得する
+        const val = document.getElementById("inpText");
+        cookie = cookie + "data1=" + val.value + ",";
+        console.log(cookie);
+    }
+
+    const btn = document.getElementById("btn");
+    btn.addEventListener("click",push);
+}
