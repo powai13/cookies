379ec3f62a8fa4bec77991bf0d36453106cd4222
powai13 <76779356+powai13@users.noreply.github.com>
2022-12-07 12:22:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ window.onload = function () {
 
         // 入力したテキストを取得する
         const val = document.getElementById("inpText");
-        cookie = cookie + "data1=" + val.value;
+        cookie = cookie +　 "data1=1";
         console.log(cookie);
     }
 
